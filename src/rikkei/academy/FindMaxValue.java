31988f41c8a0a6d2e15dbd6ee0f29f7a1e6b6320
Yuumin97 <fiesportlmtc@gmail.com>
2022-08-15 22:50:49 +0700

    k

---
+++ src/rikkei/academy/FindMaxValue.java
@@ -0,0 +1,24 @@
+package rikkei.academy;
+
+import java.util.List;
+
+public class FindMaxValue {
+    public static int findMax(List<Integer> numbers) {
+        int max = numbers.get(0);
+        for (int i = 0; i < numbers.size(); i++) {
+            if (max < numbers.get(i)) {
+                max = numbers.get(i);
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        ReadAndWriteFile readAndWriteFile = new ReadAndWriteFile();
+        String PATH_NUMBER = "D:\\MD2\\Bai16\\TH2\\TH2\\src\\rikkei\\academy\\munber";
+        String PATH_RESULT = "D:\\MD2\\Bai16\\TH2\\TH2\\src\\rikkei\\academy\\result";
+        List<Integer> numbers = readAndWriteFile.readFile(PATH_NUMBER);
+        int maxValue = findMax(numbers);
+        readAndWriteFile.writeFile(PATH_RESULT, maxValue);
+    }
+}
